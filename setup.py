e815ff9df9a6d06b8c1ad8e3c1c01954121912e4
Zeshiba <joshuaisanan@gmail.com>
2018-10-24 18:14:34 +0800

    Add files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setuptools.setup(
+    name="snipprcomparison",
+    version="0.0.1",
+    author="Joshua Isanan",
+    author_email="joshuaisanan@gmail.com",
+    description="A package used to compare two snippets of code.",
+    url="https://github.com/pypa/sampleproject",
+    packages=['snipprcomparison'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
